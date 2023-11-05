c528a3381c3fe6accc4e5a283fda0ffbaa115bd7
amir-279 <149326824+amir-279@users.noreply.github.com>
2023-11-05 20:25:29 +0330

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,20 @@
+for a in range(1,51):
+    print(a)
+for b in range(1,51):
+    print(b)
+for c in range(1,51):
+    print(c)
+for d in range(1,51):
+    print(d)
+for e in range(1,51):
+    print(e)
+for f in range(1,51):
+    print(f)
+for g in range(1,51):
+    print(g)
+for h in range(1,51):
+    print(h)
+for i in range(1,51):
+    print(i)
+for j in range(1,51):
+    print(j)
